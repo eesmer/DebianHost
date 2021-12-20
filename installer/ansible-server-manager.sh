fd55680bc55212b62c2654ad3b701958a15c7f98
erkan <erkan.esmer@beyaz.net>
2021-12-20 11:52:00 +0300

    ansible manager updated

--- installer/ansible-server-manager.sh
+++ installer/ansible-server-manager.sh
@@ -11,18 +11,24 @@ read -p "$message" readEnterKey
 
 function show_menu(){
 date
-echo "   |----------------------------|"
-echo "   |DebianHost Ansible Manager  |"
-echo "   |----------------------------|"
-echo "   | 1.add/remove machine       |"
-echo "   | 2.Ping Test                |"
-echo "   |----------------------------|"
-echo "   | 3.Package install          |"
-echo "   | 4.Package uninstall        |"
-echo "   | 5.Package update           |"
-echo "   |----------------------------|"
-echo "   |99.Exit                     |"
-echo "   |----------------------------|"
+echo "   |--------------------------------------------------|"
+echo "   | DebianHost Ansible Manager                       |"
+echo "   |--------------------------------------------------|"
+echo "   | Manage Linux Host    | Manage Windows Host       |"
+echo "   |--------------------------------------------------|"
+echo "   | 1. ping              | 20. ping                  |"
+echo "   |--------------------------------------------------|"
+echo "   | 2. install package   | 21. install   .msi        |"
+echo "   | 3. remove package    | 22. uninstall .msi        |"
+echo "   |--------------------------------------------------|"
+echo "   |                      | 23. Restart Service       |"
+echo "   |                      |---------------------------|"
+echo "   |                      | 29. Set Connection vars   |"
+echo "   |--------------------------------------------------|"
+echo "   | 50.Add Host                                      |"
+echo "   |--------------------------------------------------|"
+echo "   | 99.Exit                                          |"
+echo "   |--------------------------------------------------|"
 }
 
 function add_host(){
@@ -32,11 +38,116 @@ vim /usr/local/reman/hosts
 pause
 }
 
-function ping_test(){
-echo "Ping Test"
-PINGADDR=$(whiptail --title "Group/Host info" --inputbox "Please enter group or host name" 10 60  3>&1 1>&2 2>&3)
+function host_list() {
+egrep "^[0-9a-zA-Z]" /etc/ansible/hosts |cut -d '#' -f1 |awk '{ print $1 }' | sort | uniq > /tmp/hosts.txt
+let i=0
+W=()
+while read -r line; do
+	let i=$i+1
+	W+=($i "$line")
+done < <( cat /tmp/hosts.txt)
+IND=$(whiptail --title "Select Host" --menu "Chose one" 24 50 17 "${W[@]}" 3>&2 2>&1 1>&3)
+HOST=$(sed -n $IND\p /tmp/hosts.txt)
+}
 
-ansible $PINGADDR -m ping
+function ping_linux(){
+echo "Ping Test"
+host_list
+ansible $HOST -m ping
+pause
+}
+
+function ping_windows(){
+echo "Ping Test"
+host_list
+ansible $HOST -m win_ping
+pause
+}
+
+function install_msi(){
+echo "install .msi"
+host_list
+MSI_PATH=$(whiptail --title "msi Path" --inputbox "Where is the .msi package?" 10 60  3>&1 1>&2 2>&3)
+
+cat > /tmp/install_msi.yml <<EOF
+---
+- name: install msi
+  hosts: $HOST
+
+  tasks:
+         - name: install msi
+           win_package:
+                   path: $MSI_PATH
+                   state: present
+EOF
+ansible-playbook /tmp/install_msi.yml
+rm /tmp/install_msi.yml
+pause
+}
+
+function download_install_msi(){
+echo "download and install .msi"
+host_list
+DOWNLOAD_LINK=$(whiptail --title "MSI Download Link" --inputbox "MSI Download Link" 10 60  3>&1 1>&2 2>&3)
+DEST_PATH=$(whiptail --title "MSI Destination Path" --inputbox "MSI Destination Path" 10 60  3>&1 1>&2 2>&3)
+
+cat > /tmp/download_install_msi.yml <<EOF
+---
+- name: Download and install .msi
+  hosts: $HOST
+
+  tasks:
+         - name: download msi
+           win_get_url:
+                   url: $DOWNLOAD_LINK
+                   dest: $DEST_PATH
+         - name: install msi
+           win_package:
+                   path: $DEST_PATH
+                   state: present
+EOF
+ansible-playbook /tmp/download_install_msi.yml
+rm /tmp/download_install_msi.yml
+pause
+}
+
+function uninstall_msi(){
+echo "uninstall .msi"
+host_list
+MSI_PATH=$(whiptail --title "msi Path" --inputbox "Where is the .msi package?" 10 60  3>&1 1>&2 2>&3)
+
+cat > /tmp/uninstall_msi.yml <<EOF
+---
+- name: uninstall msi
+  hosts: $HOST
+
+  tasks:
+         - name: uninstall msi
+           win_package:
+                   path: $MSI_PATH
+                   state: absent
+EOF
+ansible-playbook /tmp/uninstall_msi.yml
+rm /tmp/uninstall_msi.yml
+pause
+}
+
+function set_conn_vars(){
+AUSER=$(whiptail --title "Ansible User" --inputbox "Please enter the Username for Windows Host connection" 10 60  3>&1 1>&2 2>&3)
+APASS=$(whiptail --title "Ansible User" --passwordbox "Please enter the Windows User Password for Windows Host connection" 10 60  3>&1 1>&2 2>&3)
+
+if [ ! -z "$AUSER" ] || [ ! -z "$APASS" ]; then
+mkdir -p /etc/ansible/group_vars
+chmod 755 /etc/ansible/group_vars
+cat > /etc/ansible/group_vars/win.yaml <<EOF
+ansible_user: $AUSER
+ansible_password: $APASS
+ansible_connection: winrm
+ansible_winrm_server_cert_validation: ignore
+ansible_winrm_transport: basic
+ansible_winrm_port: 5985
+EOF
+fi
 pause
 }
 
@@ -99,12 +210,15 @@ function read_input(){
 local c
 read -p "Please choose from the menu numbers.." c
 case $c in
-1) add_host ;;
-2) ping_test ;;
-3) package_install ;;
-4) package_uninstall ;;
+1) ping_linux ;;
+2) package_install ;;
+3) package_uninstall ;;
 5) package_update ;;
 6) system_update ;;
+20) ping_windows ;;
+21) install_msi ;;
+23) uninstall_msi ;;
+29) set_conn_vars ;;
 99) exit 0 ;;
 *)	
 echo "Selected menu not found!!"
